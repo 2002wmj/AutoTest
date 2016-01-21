9ad51d4aa44bbf186ca2301ed31bf9eaefeeccf3
2002wmj <2002wmj@163.cim>
2016-01-21 17:45:24 +0800

    当有任务处于“正在运行”状态时，无法进行新的测试，并抛出异常。

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin,messages
 from django.shortcuts import render_to_response
 from app import models
+from app.models import Pro_info
 # Register your models here.
 
 
 
 def start_test(modeladmin,request,queryset):
     #配置后台的动作菜单
-    if len(queryset) > 1:
+    start_pros = Pro_info.objects.filter(status=1)
+    if start_pros:
+        for start_pro in start_pros:
+            start_name = start_pro.name #获取处于正在运行状态的协议名称
+            messages.warning(request, u'协议"%s"正在进行测试，无法同时进行多个任务。'%(start_name))
+    elif len(queryset) > 1:
         messages.warning(request, u"目前仅支持选择一个协议进行测试。请不要勾选多个。")
     else:
         return render_to_response('autest_admin.html',{'objs': queryset,'title': u'是否开始测试?'})
